b0c503a99c3a5026b49a14722fcfc99f339f34d7
Widemar <widemarinternship@gmail.com>
2019-09-08 18:22:27 -0300

    Java Como Programar Exercicios

---
+++ src/Exercicio_2_3.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+public class Exercicio_2_3 {
+    public static void main(String[] args) {
+        int c, thisIsAVariable, q76354, value, number;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        value = input.nextInt();
+
+        System.out.println("This is a Java Program");
+
+        System.out.printf("%s%n%s%n","This is Java","Program");
+
+        number= value;
+
+        if(number!=7){
+            System.out.println("The variable number is not equal to 7");
+        }
+    }
+}
